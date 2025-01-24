fc5b6b691a6a30288a06edd7d5c3414619182fc9
wl53674 <wl53674@zut.edu.pl>
2025-01-24 11:19:21 +0100

    LAB V finished

--- PHPCustomFramework/custom-php-framework/sql/02-dog.sql
+++
@@ -1,9 +0,0 @@
-create table dog
-(
-    id      integer not null
-        constraint dog_pk
-            primary key autoincrement,
-    name    text not null,
-    breed   text not null,
-    gender  text not null
-);
